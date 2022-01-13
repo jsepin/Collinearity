cb01b93119582adca4a4006a79e6f619f9f710e3
Georgios Kazantzidis <george_kazantzidis@outlook.com>
2022-01-13 15:36:00 +0100

    documentation update

--- R/Var_decom_mat.lm.R
+++ R/Var_decom_mat.lm.R
@@ -1,9 +1,17 @@
-Var_decom_mat.lm <- function(object){
+Var_decom_mat.lm <- function(object, equilibration = TRUE){
   ## Input: a lm object 
   ## Output: the variance decompossition matrix along with the equilibrated condition indeces
   
   mat_X <- model.matrix(object)
-  eq_mat_X <- round(vdp_svd(equilibrate_matrix(mat_X)), 3)
+  
+  if(equilibration){ 
+    
+    eq_mat_X <- round( vdp_svd( equilibrate_matrix( mat_X )), 3)
+    
+  } else{
+    
+    eq_mat_X <- round( vdp_svd( mat_X ), 3)
+  }
   
   return(eq_mat_X)
 }
