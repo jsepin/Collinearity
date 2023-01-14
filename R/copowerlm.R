2915efded49fba315aa96bb000cf8b135534df35
jsepin <jerome.sepin@uzh.ch>
2023-01-14 10:03:01 +0100

    update

--- R/copowerlm.R
+++ R/copowerlm.R
@@ -1,16 +1,33 @@
 # R: Function to determine sample size or power
 #' @param alpha Significance level
-#' @param n Sample size
-#' @param power Power
+#' @param n Sample size. Default is NULL.
+#' @param power Power. Default is NULL.
 #' @param Delta Relevant difference
 #' @param sigma Experimental error
-#' @param p Number of parameters in whole model
-#' @param voilen Variable Of Interest length
-#' @param trouble Diagonal entry of the inverted squared equilibrated design matrix
+#' @param p Number of parameters in whole model. Default is 3.
+#' @param voilen Variable Of Interest length. Default is 0.5.
+#' @param trouble Diagonal entry of the inverted squared equilibrated design matrix. Default is 1.
+#' @param m lm model. Default is NULL.
+#' @param voi Variable of interest. Default is NULL.
 #'
 #' @return Returns the power or sample size
 
-n_finder <- function(power=NULL,n = NULL ,alpha=0.05, Delta, sigma, p=3 , voilen=0.5, trouble = 1){
+copowerlm <- function(power=NULL,n = NULL ,alpha=0.05, Delta, sigma, p=3 , voilen=0.5, trouble = 1,m = NULL, voi = NULL){
+  # if lm model is given
+  if(!is.null(m)){
+    X <- model.matrix(m)
+    # search voi
+    if(!(voi %in% colnames(X)) || is.null(voi)){
+      stop(paste0("voi not found! \n Try: ", paste0(gsub("[`]","",colnames(X)),collapse = ", ") ) )
+    }
+    E <- equilibrate_matrix(X)
+    trouble <- solve(t(E) %*% E)[voi,voi]
+    voilen  <- var(X[,voi]) + mean(X[,voi])^2
+    sigma <- stats::sigma(m)
+    p <- ncol(X)
+    if(is.null(power)){n <- nrow(X)}
+  }
+  
   # power function
   myFpower <- function(alpha, n, Delta, sigma, p , voilen, trouble){
     ncp <- ((n *voilen*  Delta^2)/(sigma^2))  * (1/trouble)
@@ -31,8 +48,8 @@ n_finder <- function(power=NULL,n = NULL ,alpha=0.05, Delta, sigma, p=3 , voilen
       # power calculation
       tryCatch({
         result <- myFpower(alpha, n,Delta, sigma, p, voilen, trouble) },
-        warning = function(w){result <<- NA},
-        error  = function(e){result <<- NA}
+        warning = function(w){result <<- NA; print(warnings()) },
+        error  = function(e){result <<- NA; print("Error! Return NA") }
       )
       return(result)
       })
@@ -47,9 +64,9 @@ n_finder <- function(power=NULL,n = NULL ,alpha=0.05, Delta, sigma, p=3 , voilen
                             "p"=p, "voilen"=voilen, "trouble"=trouble)
   
   re <-apply(conditions, 1, function(x) {
+    x <- unlist(x) 
     power <- x[1]; alpha <- x[2]; Delta <- x[3]; sigma <- x[4]; p <- x[5];
     voilen <- x[6]; trouble <- x[7]
-    
     
     #loss function
     f <- function(power, alpha, n, Delta, sigma, p, voilen, trouble){
@@ -61,8 +78,8 @@ n_finder <- function(power=NULL,n = NULL ,alpha=0.05, Delta, sigma, p=3 , voilen
                         power = power, alpha = alpha, Delta=Delta,p=p,
                         sigma = sigma, voilen=voilen, trouble = trouble
       )$root },
-      warning = function(w){result <<- NA},
-      error  = function(e){result <<- NA}
+      warning = function(w){result <<- NA; print(summary(warnings()) )},
+      error  = function(e){result <<- NA; print("Error! Return NA")}
     )
     return(result)
   })
@@ -73,4 +90,5 @@ n_finder <- function(power=NULL,n = NULL ,alpha=0.05, Delta, sigma, p=3 , voilen
   
   return(out)
 }
+
 
--- R/equilibrate_matrix.R
+++ R/equilibrate_matrix.R
@@ -1,5 +1,6 @@
 equilibrate_matrix <- function(object){
   # Input is a design matrix X
   eX <- object %*% diag(1 / sqrt(diag(t(object) %*% object)))
+  colnames(eX) <- colnames(object)
   return(eX)
 }
